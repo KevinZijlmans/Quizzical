66f07467bef9d1623f4277487087ba729e490c69
kevin <kevin@ditisabc.nl>
2019-11-21 10:55:25 +0100

    Added question-modal component

--- src/components/question-form/index.js
+++ src/components/question-form/index.js
@@ -4,8 +4,17 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { FileInput } from '@8base/file-input';
 import { toast } from 'react-toastify';
+
 import 'react-tagsinput/react-tagsinput.css';
 import './form.css';
+
+const QUESTION_CREATE_MUTATION = gql`
+  mutation QuestionCreate($data: QuestionCreateInput!) {
+    questionCreate(data: $data) {
+      id
+    }
+  }
+`;
 
 const QuestionForm = ({ closeModal, questionCreate }) => {
   const [questionForm, setQuestion] = useState({
@@ -14,40 +23,45 @@ const QuestionForm = ({ closeModal, questionCreate }) => {
     image: {},
     answer: '',
   });
-
   const inputProps = {
     placeholder: 'Add an option and press enter',
     className: 'question-input',
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     await questionCreate({ variables: { data: questionForm } });
     closeModal();
     toast('Your question has been created successfully');
-};
   };
+
   const handleInputChange = (event) => {
     event.preventDefault();
     const {
       target: { name, value },
     } = event;
+
     setQuestion({
       ...questionForm,
       [name]: value,
     });
   };
+
   const handleTagsChange = (options) => {
     setQuestion({
       ...questionForm,
       options,
     });
   };
+
   const handleImageChange = (value) => {
+    console.log(value);
     setQuestion({
       ...questionForm,
       image: { create: value },
     });
   };
+
   return (
     <form action="" id="question-form" onSubmit={handleSubmit}>
       <input
@@ -93,6 +107,7 @@ const QuestionForm = ({ closeModal, questionCreate }) => {
           </div>
         )}
       </FileInput>
+
       <div className="submit-area">
         <button className="submit-button" type="submit">
           Create Question
@@ -102,11 +117,6 @@ const QuestionForm = ({ closeModal, questionCreate }) => {
   );
 };
 
-const QUESTION_CREATE_MUTATION = gql`mutation QuestionCreate($data: QuestionCreateInput!) {
-    questionCreate(data: $data) {
-      id
-    }
-  }`;
 export default graphql(QUESTION_CREATE_MUTATION, {
   name: 'questionCreate',
 })(QuestionForm);
---
+++ src/components/question-modal/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Modal from "react-modal";
+import QuestionForm from "../question-form";
+import "./modal.css";
+import Close from "./cancel.svg";
+const customStyles = {
+  content: {
+    top: "40%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    border: "none",
+    width: "450px",
+    height: "400px",
+    background: "#f7f8f9",
+    boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)"
+  }
+};
+Modal.setAppElement("#root");
+const QuestionModal = ({ isOpen, closeModal }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Question Modal"
+    >
+      <div className="modal-header">
+        <p />
+        <p>Add a question</p>
+        <span onClick={closeModal} className="close">
+          <img src={Close} alt="Press button to close modal" />
+        </span>
+      </div>
+      <QuestionForm closeModal={closeModal} />
+    </Modal>
+  );
+};
+export default QuestionModal;
