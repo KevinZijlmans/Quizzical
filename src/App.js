f146f0f1b72ddcd11e23333f20f7d510b44145e4
kevin <kevin@ditisabc.nl>
2019-11-21 11:08:12 +0100

    Changed the App.js to show everything.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
-
+import { toast } from 'react-toastify'
 import Questions from './components/questions';
+import QuestionModal from './components/question-modal';
+import Add from './add.svg'
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
+toast.configure(); // -- 1
 const client = new ApolloClient({
   uri: 'https://api.8base.com/ck37ef1pe000201jv5njf6hsf'
 })
 
 function App() {
+const [modalOpen, setModalOpen] = useState(false); // --2
+const closeModal = () => {
+  setModalOpen(false); // -- 3
+}
+
   return (
     <ApolloProvider client={client}>
     <div className="App">
-      <Questions/>
+      <header>
+        <div>Game of Thrones Quizzical</div>
+      </header>
+      <Questions />
+      <button className="add-question" onClick={_ => setModalOpen(true)}>
+        <img src={Add} alt="Click to create a new question"/>
+      </button>
+      <QuestionModal // --- 5
+          isOpen={modalOpen}
+          closeModal={closeModal}
+        />
     </div>
     </ApolloProvider>
   );
--- src/components/question-modal/index.js
+++ src/components/question-modal/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Modal from "react-modal";
 import QuestionForm from "../question-form";
-import "./modal.css";
+import "./question-modal.css";
 import Close from "./cancel.svg";
 const customStyles = {
   content: {
